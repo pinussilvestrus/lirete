1b051d66a3b673516414f948073f82cbb50bf572
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-12-17 21:32:56 +0100

    create basic table and loading spinner

--- src/core/components/Home/index.jsx
+++ src/core/components/Home/index.jsx
@@ -2,13 +2,22 @@ require('../../styles/base.scss');
 require('../../styles/layout.scss');
 require('./index.scss');
 import Actions from '../../actions';
+import Table from '../Table';
+import Loader from 'react-loader';
 
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
+    this.state = {
+      loaded: true
+    };
 	}
 
   handlePlot(e) {
+    this.setState({
+      loaded: false
+    });
+
     Actions.plot({});
   }
 
@@ -16,12 +25,29 @@ class Home extends React.Component {
     e.target.style.display = "none";
   }
 
+  getTableHead() {
+    return [
+      "x",
+      "y"
+    ]
+  }
+
+  getTableBody() {
+    return [
+      [1, 2],
+      [3, 4]
+    ]
+  }
+
 	render() {
 		return (
 			<div className="container home">
         <h2>Lirete (Linear Regression Template)</h2><br></br>
-        <button className="btn btn-default btn-large" onClick={this.handlePlot.bind(this)}>Plot</button><br></br>
-        <img src="plot.png" onError={this.handleImageNotFound.bind(this)}></img>
+        <Table head={this.getTableHead()} body={this.getTableBody()} />
+        <Loader loaded={this.state.loaded}>
+          <button className="btn btn-success btn-large" onClick={this.handlePlot.bind(this)}>Plot</button><br></br>
+          <img src="plot.png" onError={this.handleImageNotFound.bind(this)}></img>
+        </Loader>
 			</div>
 		);
 	}
